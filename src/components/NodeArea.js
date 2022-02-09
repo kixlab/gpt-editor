8bcfe335a1940db83de847976cb1326d86a31132
tsook <kimts0830@gmail.com>
2022-02-10 04:13:35 +0900

    Allow holding generation button, but need to fix not updating

--- src/components/NodeArea.js
+++ src/components/NodeArea.js
@@ -9,8 +9,11 @@ const colors = [
 
 function NodeArea(props) {
     const containerRef = useRef(null);
+    const keyTimer = useRef(null);
+
     const [spans, setSpans] = useState([]);
     const [caretPosition, setCaretPosition] = useState({start: 0, end: 0});
+    const [keyPressed, setKeyPressed] = useState(null);
 
     useEffect(() => {
         if(props.sentenceIds.length == spans.length) return;
@@ -37,17 +40,23 @@ function NodeArea(props) {
         CaretPositioning.restoreSelection(document.getElementById("editable"), caretPosition);
     }, [caretPosition])
 
-    function handleKeyDown(e) {
-        if(props.isAlt) {
-            if (e.key === "Enter") {
-                props.handleGenerate(props.nodeId, false);
-            } else if (e.key === "Backspace") {
-                props.handleDelete(props.nodeId);
-            }
+    useEffect(() => {
+        if(keyPressed != null) {
+            keyTimer.current = setTimeout(() => {
+                console.log(keyPressed.count);
+                setKeyPressed({
+                    key: keyPressed.key,
+                    count: keyPressed.count + 1
+                });
+                placeholderGenerate(keyPressed.count + 1);
+            }, 1000);
+        } else {
+            clearTimeout(keyTimer.current);
         }
-    }
+    }, [keyPressed]);
 
     function handleChange(e) {
+        console.log("change");
         let targetValue =  e.currentTarget.textContent;
         //save caret position(s), so can restore when component reloads
         let savedCaretPosition = CaretPositioning.saveSelection(e.currentTarget);
@@ -69,11 +78,48 @@ function NodeArea(props) {
         setCaretPosition(savedCaretPosition);
     }
 
+    function handleKeyDown(e) {
+        if(props.isAlt) {
+            if (e.key === "Enter" && keyPressed == null) {
+                setKeyPressed({key: e.key, count: 0});
+                //props.handleGenerate(props.nodeId, false);
+            } else if (e.key === "Backspace") {
+                props.handleDelete(props.nodeId);
+            }
+        }
+    }
+
+    function handleKeyUp(e) {
+        if(keyPressed != null && e.key === "Enter") {
+            setKeyPressed(null);
+        }
+    }
+
+    function placeholderWord() {
+        return " " + "_".repeat(Math.floor(Math.random() * 4) + 3);  
+    }
+
+    function placeholderGenerate(count) {
+        const nextSentenceId = parseInt(Object.keys(props.sentencesText).at(-1)) + count;
+        var newSpans = [...spans];
+        var placeholder = placeholderWord();
+        if(count > 0) {
+            placeholder += placeholderWord().repeat(Math.floor(Math.random() * 10) + 16) + ".";
+        }
+
+        newSpans.push(
+            <span key={nextSentenceId} style={{backgroundColor: colors[nextSentenceId % colors.length]}}>{placeholder}</span>
+        )
+        
+        setSpans(newSpans);
+    }
+
     return (
         <TextContainer 
             id="editable"
             ref={containerRef} 
             onKeyDown={handleKeyDown}
+            onKeyUp={handleKeyUp}
             onFocus={() => props.handleFocus(props.nodeId, 0)}
             onInput={handleChange}
             contentEditable={true}
