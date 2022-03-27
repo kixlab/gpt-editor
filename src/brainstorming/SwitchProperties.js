9540f5552de866d7df4d4f8488b4f0a88b7b264e
tsook <kimts0830@gmail.com>
2022-03-27 20:42:08 +0900

    Fix baggage to babbage

--- src/brainstorming/SwitchProperties.js
+++ src/brainstorming/SwitchProperties.js
@@ -58,14 +58,14 @@ function SwitchProperties(props) {
                         <select className="dropdown" value={properties[name]} data-property={name} onChange={handleChange}>
                             <option value="text-davinci-002">text-davinci-002</option>
                             <option value="text-curie-001">text-curie-001</option>
-                            <option value="text-baggage-001">text-baggage-001</option>
+                            <option value="text-babbage-001">text-babbage-001</option>
                             <option value="text-ada-001">text-ada-001</option>
                             <option value="text-davinci-001">text-davinci-001</option>
                             <option value="davinci-instruct-beta">davinci-instruct-beta</option>
                             <option value="davinci">davinci</option>
                             <option value="curie-instruct-beta">curie-instruct-beta</option>
                             <option value="curie">curie</option>
-                            <option value="baggage">baggage</option>
+                            <option value="babbage">babbage</option>
                             <option value="ada">ada</option>
                         </select>
                     )
